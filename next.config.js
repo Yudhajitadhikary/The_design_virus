32a7208fe48f5f78c19de6b5060b23348f9c954d
yudhajit <yudhajitadhikary@gmail.com>
2025-01-14 12:28:09 +0530

    Added fixs

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  images:{
+      unoptimized: false,
+      domains:['img.icons8.com']
+  },
+  output:'export',
+  distDir:'dist',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+}
